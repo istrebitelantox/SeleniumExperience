bec5003ed63790afa886608e101a6fbc8f14457d
istrebitelantox <geekpook@gmail.com>
2023-01-13 18:00:52 +0300

    Begin

---
+++ src/test/java/Tust/SeleniumFirst.java
@@ -0,0 +1,25 @@
+package Tust;
+
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SeleniumFirst {
+    WebDriver webDriver;
+    @BeforeClass
+    public static void setup() {
+        //определение пути до драйвера и его настройка
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        //создание экземпляра драйвера
+        WebDriver driver = new ChromeDriver();
+        //окно разворачивается на полный экран
+        driver.manage().window().maximize();
+        //задержка на выполнение теста = 10 сек.
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //получение ссылки на страницу входа из файла настроек
+        driver.get(ConfProperties.getProperty("loginpage"));
+    }
+}
